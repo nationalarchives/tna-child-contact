4b515f4022c8f00c7564d8ea9bb33e7d605e09d6
domingobishop <domingobishop@gmail.com>
2020-10-13 09:47:05 +0100

    aws proxy check

--- src/setThemeGlobals.php
+++ src/setThemeGlobals.php
@@ -20,6 +20,7 @@ function setThemeGlobals($environment = null) {
             $pre_crumbs = array('Contact us' => '/');
             break;
         case 'external':
+        case 'aws_public':
             $pre_crumbs = array(
                 'Contact us' => '/contact-us/'
             );
